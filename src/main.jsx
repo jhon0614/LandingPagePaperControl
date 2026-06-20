ea269c4294d2683378bfae6b9356f537328dd288
Jhon Ricardo Rios Cuervo <jhonri.0614@gmail.com>
2026-06-20 16:24:01 -0500

    retirando información duplicada durante merge con proyecto kelly

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { HashRouter } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
